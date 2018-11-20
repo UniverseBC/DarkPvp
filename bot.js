c5465cf7421327522e3127c29d3e43d406168946
UniverseBC <44294510+UniverseBC@users.noreply.github.com>
2018-11-20 18:27:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,8 +39,8 @@ client.on('message', message => {
   user.addRole(${act})
   }
 });
-  const devs = ["your id"];
-const adminprefix = [" type a prefix "];
+  const devs = ["502812393907290112"];
+const adminprefix = ["."];
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developer.includes(message.author.id)) return;
@@ -52,15 +52,15 @@ client.on('message', message => {
      if (message.content === (adminprefix + "leave")) {
     message.guild.leave();        
   } else  
-  if (message.content.startsWith(adminprefix + 'wt')) {
+  if (message.content.startsWith(adminprefix + 'setwatching')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
       message.channel.send(`**✅   ${argresult}**`)
   } else 
-  if (message.content.startsWith(adminprefix + 'ls')) {
+  if (message.content.startsWith(adminprefix + 'setlisting')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.send(`**✅   ${argresult}**`)
   } else 
-  if (message.content.startsWith(adminprefix + 'st')) {
+  if (message.content.startsWith(adminprefix + 'setstreaming')) {
     client.user.setGame(argresult, "https://www.twitch.tv/idk");
       message.channel.send(`**✅**`)
   }
@@ -92,65 +92,9 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {
   }
 });
   
-  var stopReacord = true;
-var reactionRoles = [];
-var definedReactionRole = null;
-
-client.on("message", async message => {
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-    if(message.author.bot) return;
-    if(message.content.indexOf(prefix) !== 0) return;
-    if (command == "autoc") {
-      if(!message.channel.guild) return message.reply(`**this ~~command~~ __for servers only__**`);
-      if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("sorry you can't do this");
-      if(!args[0] || args[1]) return message.channel.send(`\`\`\`${prefix}autoC <role-name>\`\`\``);
-      var role = message.guild.roles.find( role => { return role.name == args[0] });
-      if(!role) return message.channel.send(`no role with name ${definedRoleName} found, make sure you entered correct name`);
-      if(definedReactionRole != null  || !stopReacord) return message.channel.send("another reaction role request is running");
-      message.channel.send(`now go and add reaction in the message you want for role ${role.name}`);
-      definedReactionRole = role;
-      stopReacord = false;
-    }     
-})
-client.on('raw', raw => {
-  if (!['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(raw.t)) return;
-  var channel = client.channels.get(raw.d.channel_id);
-  if (channel.messages.has(raw.d.message_id)) return;
-  channel.fetchMessage(raw.d.message_id).then(message => {
-    var reaction = message.reactions.get( (raw.d.emoji.id ? `${raw.d.emoji.name}:${raw.d.emoji.id}` : raw.d.emoji.name) );
-    if (raw.t === 'MESSAGE_REACTION_ADD') return client.emit('messageReactionAdd', reaction, client.users.get(raw.d.user_id));
-    if (raw.t === 'MESSAGE_REACTION_REMOVE') return client.emit('messageReactionRemove', reaction, client.users.get(raw.d.user_id));
-  });
-});
-client.on('messageReactionAdd', (reaction, user) => {
-    if(user.id == client.user.id) return;
-    if(!stopReacord) {
-      var done = false;
-      reactionRoles[reaction.message.id] = { role: definedReactionRole, message_id: reaction.message.id, emoji: reaction.emoji};
-      stopReacord =  true;
-      definedReactionRole = null;
-      reaction.message.react(reaction.emoji.name)
-      .catch(err => {done = true; reaction.message.channel.send(`sorry i can't use this emoji but the reaction role done! anyone react will get the role!`)})
-      if(done) reaction.remove(user); 
-    } else {
-      var request = reactionRoles[reaction.message.id];
-      if(!request) return;
-      if(request.emoji.name != reaction.emoji.name) return reaction.remove(user);
-      reaction.message.guild.members.get(user.id).addRole(request.role);
-    }
-}) 
-client.on('messageReactionRemove', (reaction, user) => {
-  if(user.id == client.user.id) return;
-  if(!stopReacord) return;
-  let request = reactionRoles[reaction.message.id];
-  if(!request) return;
-  reaction.message.guild.members.get(user.id).removeRole(request.role);
-});
-
 
 lient.on('message', warn => {
-  var prefix = ".";
+  var prefix = "$";
   let log = warn.guild.channels.find('name', "log")
   let all = warn.guild.channels.find('name', "public-chat")
   let user = warn.mentions.members.first();
@@ -182,43 +126,6 @@ client.on("message", message => {
 };     
 });
 
-const afk = require('./afk.json');
-client.on('message',async rebel => {
-      if(rebel.author.bot) return;
-  if (afk[rebel.author.id]) {
-    delete afk[rebel.author.id];
-    if (rebel.member.nickname === null) {
-      msg.channel.send("أهلا بك , <@"+rebel.author.id+"> أنأ **أحاول** أن أزيل عنك الأفك.");     } else {
-      rebel.member.setNickname(rebel.member.nickname.replace(/(\[AFK\])/,''));
-      rebel.channel.send("أهلا بك , <@"+rebel.author.id+"> تم فك عنك الأفك بسبب رجوعك."); 
-    }
-    fs.writeFile("./afk.json", JSON.stringify(afk), (err) => {if (err) console.error(err);});
-} else {
-    if (rebel.content.startsWith(prefix + 'afk ')||rebel.content === prefix + 'afk') {
-      rebel.member.setNickname("[AFK] " + rebel.member.displayName);
-      let args1 = rebel.content.split(' ').slice(1);
-      if (args1.length === 0) {
-        afk[rebel.author.id] = {"reason": true}; 
-        rebel.reply("** لقد وضعتك بوضع الأفك **")
-      } else {
-        afk[rebel.author.id] = {"reason": args1.join(" ")}; // with reason
-        rebel.reply("لقد أضفتك للأفك بسبب "+ args1.join(" ") + ".")
-      }
-      fs.writeFile("./afk.json", JSON.stringify(afk), (err) => {if (err) console.error(err);});   
-  }
-}
-         var mentionned = rebel.mentions.users.first();
-if(rebel.mentions.users.size > 0) return ;
-if (afk[rebel.mentions.users.first().id]) {
-if (afk[rebel.mentions.users.first().id].reason === true) {
-rebel.channel.send(`**<@!${mentionned.id}> مأفك** `);
-}else{
-rebel.channel.send(`**<@!${mentionned.username}> مأفك , سبب الأفك \n ${afk[rebel.mentions.users.first().id].reason}**`);
-}
-} 
-});
-
-
 client.on('message', message => {
         var  user = message.mentions.users.first() || message.author;
     if (message.content.startsWith("$avatar")) {
@@ -243,7 +150,7 @@ client.on('message' , message => {
             .setAuthor(client.user.username,client.user.avatarURL)
             .setThumbnail(client.user.avatarURL)
             .setColor('RANDOM')
-            .setTitle('Arabe Codes.')
+            .setTitle('AcidLands Network.')
             .addField('**My Ping**' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
             .addField('**RAM Usage**', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true)
             .addField('**Servers**', [client.guilds.size], true)
@@ -257,19 +164,13 @@ client.on('message' , message => {
             .addField('**Platform**' , `[ ${process.platform} ]` , true)
                   .addField('**My Prefix**' , `[ ${prefix} ]` , true)
                   .addField('**My Language**' , `[ Java Script ]` , true)
-                  .setFooter('By |'SaâaD .#1001')
+                  .setFooter('By |'Void >> AcidLands')
     })
 }
-});
-client.on("ready", () => {
-  function lol() {
-    client.guilds.get('id server').roles.find("name", "اسم رتبة").setColor("RANDOM");
-  }
-  setInterval(lol, 10000);
-})
+
 client.on("message", (message) => {
-   var prefix = "-"/// البرفكس حقك
-if (message.content.startsWith(prefix + "ticket")) {
+   var prefix = "$"
+if (message.content.startsWith(prefix + "new")) {
         const reason = message.content.split(" ").slice(1).join(" ");     
         if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
         if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
@@ -303,9 +204,9 @@ if (message.content.startsWith(prefix + "ticket")) {
 if (message.content.startsWith(prefix + "close")) {
         if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
  
-        message.channel.send(`هل أنت متأكد؟ بعد التأكيد ، لا يمكنك عكس هذا الإجراء!\n للتأكيد ، اكتب\`*confirm\`. سيؤدي ذلك إلى مهلة زمنية في غضون 10 ثوانٍ وإلغائها`)
+        message.channel.send(`Are you sure you want to remove ticket ? type $confirm`)
             .then((m) => {
-                message.channel.awaitMessages(response => response.content === '*confirm', {
+                message.channel.awaitMessages(response => response.content === '$confirm', {
                         max: 1,
                         time: 10000,
                         errors: ['time'],
